faaacb00dfb2db478a778b92a2053f78fe7fdd69
Amatriciana <amatriciana913@gmail.com>
2022-04-13 11:18:03 +0900

    画面遷移実装

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,40 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'counter.dart';
+import 'bmi_calc.dart';
+
+final bottomNavigationProvider =
+    StateProvider<BottomNav>((ref) => BottomNav.counter);
+
+enum BottomNav {
+  counter,
+  bmiCalc,
+}
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final bottomNavState = ref.watch(bottomNavigationProvider);
+    final bottomNavState2 = ref.watch(bottomNavigationProvider.notifier);
+
+    final _pageList = [
+      const CounterApp(),
+      const BmiCalcApp(),
+    ];
+
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: const Text('testApp')),
-      body: const CounterApp(),
+      body: _pageList[bottomNavState.index],
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) => {},
+        onTap: (int selectIndex) {
+          bottomNavState2.state = BottomNav.values[selectIndex];
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.add),
