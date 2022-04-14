48b2af4ef9a895dfb9d70a13a98c40e1efe48b1f
Amatriciana <amatriciana913@gmail.com>
2022-04-14 19:38:47 +0900

    rollback

--- lib/bmi_history.dart
+++ lib/bmi_history.dart
@@ -1,62 +1,69 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'bmi_calc.dart';
 
-final listStreamProvider = FutureProvider.autoDispose((ref) async {
-  final prefs = await SharedPreferences.getInstance();
-  List formList = [];
-  if (prefs.getString('height') != null) {
-    formList.add(prefs.getString('height'));
-  }
-  if (prefs.getString('weight') != null) {
-    formList.add(prefs.getString('weight'));
-  }
-  print(formList);
-  return formList;
-});
+final listProvider = StateNotifierProvider((ref) => ListNotifier());
 
-class BmiHistory extends ConsumerWidget {
+class ListNotifier<List, String> extends StateNotifier {
+  ListNotifier() : super([]);
+
+  void initialize() {
+    state = ['uuu', 'eee'];
+  }
+
+  Future<void> getHeightPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    state.add(prefs.getString('height') ?? 'aaa');
+    state.add(prefs.getString('weight') ?? 'iii');
+    print(state);
+  }
+}
+
+class BmiHistory extends HookConsumerWidget {
   const BmiHistory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final listStream = ref.watch(listStreamProvider);
-    return listStream.when(
-      data: (data) => Scaffold(
-        appBar: AppBar(
-          title: const Text('BMI Calculate History'),
-        ),
-        body: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    width: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              child: ListTile(
-                title: Text(
-                  data[index],
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                  ),
-                ),
-                onTap: () {
-                  print(data[index]);
-                },
-              ),
-            );
-          },
-        ),
+    final listState = ref.watch(listProvider.notifier);
+    useEffect(() {
+      listState.initialize();
+      listState.getHeightPrefs();
+      print(listState.state);
+      return null;
+    }, []);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculate History'),
       ),
-      error: (error, stack) => const Text('error'),
-      loading: () => const Center(child: CupertinoActivityIndicator()),
+      body: ListView.builder(
+        itemCount: listState.state.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            child: ListTile(
+              title: Text(
+                listState.state[index],
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+              onTap: () {
+                print(listState.state[index]);
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
