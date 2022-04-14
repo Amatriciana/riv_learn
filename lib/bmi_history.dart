cc5d6bd297476f6e78e2c3eb6b262df6143d9f5c
Amatriciana <amatriciana913@gmail.com>
2022-04-14 16:22:02 +0900

    PC入れ替え

--- lib/bmi_history.dart
+++ lib/bmi_history.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'bmi_calc.dart';
 
-class BmiHistory extends StatelessWidget {
+final listProvider = StateNotifierProvider((ref) => ListNotifier());
+
+class ListNotifier<List, String> extends StateNotifier {
+  ListNotifier() : super([]);
+
+  Future<void> getHeightPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    state <= prefs.getString('height') ?? 'aaa';
+    state <= prefs.getString('weight') ?? 'iii';
+  }
+}
+
+class BmiHistory extends HookConsumerWidget {
   const BmiHistory({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final listState = ref.watch(listProvider.notifier);
+    final List listViewList = listState.state;
+
+    useEffect(() {
+      Future(() async {
+        listState.getHeightPrefs();
+        return null;
+      });
+    });
     return Scaffold(
       appBar: AppBar(
         title: const Text('BMI Calculate History'),
+      ),
+      body: ListView.builder(
+        itemCount: listState.state.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            child: ListTile(
+              title: Text(
+                listState.state[index],
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ),
+              ),
+              onTap: () {
+                print(listViewList[index]);
+              },
+            ),
+          );
+        },
       ),
     );
   }
